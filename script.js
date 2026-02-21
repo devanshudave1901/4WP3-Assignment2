99f6ca67f8f87de62b4ac933825c15c410cd239b
Devanshu <devanshudave022@gmail.com>
2026-02-21 12:04:20 -0500

    Add initial HTML structure and serve it via Express
    Added background Image and the white card where form will be placed

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/app',(req,res)=>{
-    res.send('Testing app')
-})
 
+
+app.get("/app", function (req,res){
+    res.sendFile(__dirname + "/index.html")
+})
+app.get("/images/background1.jpg", function (req,res){
+    res.sendFile(__dirname + "/images/background1.jpg")
+});
 app.listen(3000,()=>{
     console.log('Example app listening on port 3000!')
 })
