484b41e5034617e73c7fc040faafca2602f1c5ba
Devanshu <devanshudave022@gmail.com>
2026-02-23 00:24:18 -0500

    Add axios for API requests and update submit route to fetch recipes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const axios = require('axios');
+
+const {response} = require("express");
 const app = express()
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
@@ -13,7 +16,22 @@ app.listen(3000,()=>{
     console.log('Example app listening on port 3000!')
 })
 
-app.post("/submit", function (req,res){
-    console.log(req.body)
-    res.send("Data received")
+app.post("/submit", async function (req, res) {
+
+
+
+    const apikey= 'c6e19990e5fc4c9694540b9247b0c56e';
+    const query = 'pasta';
+    const number = 1;
+    const response1 = await axios.get('https://api.spoonacular.com/recipes/complexSearch?', {
+        params: {
+            apiKey: apikey,
+            query: query,
+            number: number
+        }
+    });
+    console.log(response1.data);
+
+
+
 })
